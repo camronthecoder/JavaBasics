411428f0a7ae24cd1a7b55bf7e5e7b35ca8e2424
camronthecoder <106614872+camronthecoder@users.noreply.github.com>
2022-06-30 18:08:05 -0400

    1

--- src/com/careerdevs/namegenerator/Main.java
+++ src/com/careerdevs/namegenerator/Main.java
@@ -1,2 +1,14 @@
-package com.careerdevs.namegenerator;public class Main {
-}
+package com.careerdevs.namegenerator;
+
+public class Main {
+    public static void main(String[] args) {
+        generateFullName();}
+
+        private static void generateFullName(){
+        String firstName = "Cameron";
+        String lastName = "Haley";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+        }
+
+    }
