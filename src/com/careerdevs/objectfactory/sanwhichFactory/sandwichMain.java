c7f6807d0f17cf489eb0ad47282b19aecd117b80
camronthecoder <106614872+camronthecoder@users.noreply.github.com>
2022-07-12 13:27:30 -0400

    3

--- src/com/careerdevs/objectfactory/sanwhichFactory/sandwichMain.java
+++ src/com/careerdevs/objectfactory/sanwhichFactory/sandwichMain.java
@@ -1,17 +1,46 @@
 package com.careerdevs.objectfactory.sanwhichFactory;
 
 import com.careerdevs.objectfactory.UI;
+import com.careerdevs.objectfactory.airplaneFactory.Airplane;
+
+import java.util.ArrayList;
 
 public class sandwichMain {
     public static void main(String[] args) {
+        ArrayList<sandwich> sandWichList = new ArrayList<>();
         System.out.println("sandwich factory\n\n");
-        int sandwichSize = UI.readInt("What is the size of the sanwhich in feet ?",0, 100);
+
+        while(true){
+            System.out.println("\nMenu\n" + "_".repeat(20));
+            System.out.println("1) Create A sandwich\n");
+            System.out.println("2) View Order");
+            System.out.println("3) Exit the program");
+
+            byte menuChoice = UI.readByte("Choose An option",(byte)1, (byte)3);
+            if (menuChoice ==3 ) break;
+            else if (menuChoice == 2) {
+                if (sandWichList.size()==0){
+                    System.out.println("There are no sandwiches currently in the order :(");
+                    continue;
+                }}
+                else {
+                    System.out.println("Current Order");
+                    for (int i = 0; i < sandWichList.size(); i++) {
+                        System.out.println(i + 1 + ")"+ sandWichList.get(i) + "\n");}
+                    System.out.println("-".repeat(20));
+                    continue;
+                    }
+                    sandwich generatedSandwich = createSandwich();
+                    sandWichList.add(generatedSandwich);
+                    System.out.println("\n\n Created Sanwich : \n" + generatedSandwich);
+                }
+                System.out.println("\n thank you for using the program");
+            }
+        private static sandwich createSandwich() {
+        int sandwichSize = UI.readInt("What is the size of the sandwich in inches ?",0, 12);
         String breadType = UI.readString("What type of bread would you like?");
         String meatType = UI.readString("What type of meat?");
         String cheeseType = UI.readString("What type of cheese?");
         boolean cutType= UI.readBoolean("You want this sandwich cut in half. ");
-
-        sandwich generateSandwich = new sandwich(sandwichSize, breadType, meatType, cheeseType, cutType);
-        System.out.println(generateSandwich);
-    }
-}
+        return new sandwich(sandwichSize,breadType,meatType,cheeseType,cutType);
+    }}
--- src/com/careerdevs/objectfactory/songFactory/songMain.java
+++ src/com/careerdevs/objectfactory/songFactory/songMain.java
@@ -1,14 +1,14 @@
 package com.careerdevs.objectfactory.songFactory;
 
-import com.careerdevs.objectfactory.ui;
+import com.careerdevs.objectfactory.UI;
 
 public class songMain {
     public static void main(String[] args) {
         System.out.println("song factory\n\n");
-        String title = ui.readString("What is the song name?");
-        String artist = ui.readString("what is the asrtist name?");
-        String genre = ui.readString("what is the genre?");
-        int seconds = ui.readInt("How many seconds is it?",0,1000);
+        String title = UI.readString("What is the song name?");
+        String artist = UI.readString("what is the artist name?");
+        String genre = UI.readString("what is the genre?");
+        int seconds = UI.readInt("How many seconds is it?",0,1000);
         song generateSong = new song(title, artist, genre, seconds);
         System.out.println(generateSong);
     }
